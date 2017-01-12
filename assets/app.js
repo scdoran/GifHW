03f147e1031c4b42b85f4fd89f6163fdf9b6da02
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2017-01-11 21:44:46 -0500

    Javascript and jQeuery document is acting weird.

--- assets/app.js
+++ assets/app.js
@@ -2,16 +2,18 @@
 $(document).ready(function() {
 
 	// Array where movie names are stored.
-	var buttonArray= ["Re-Animator", "Alien", "Texas Chainsaw Massacre", "The Fly", "They Live", "Dead Alive", "Samurai Cop", "Riki-Oh"];
+	var buttonArray= ["Re-Animator", "Evil Dead", "Texas Chainsaw Massacre", "The Fly", "They Live", "Dead Alive", "Samurai Cop", "Riki-Oh"];
 
+// function newButton(){
 // For loop to create buttons for each element in the array.
-	for (var i = 0; i < buttonArray.length; i++) {
-		
-		var newButton = $("<button>").attr("data-movie");
+		for (var i = 0; i < buttonArray.length; i++) {
+			
+			var newButton = $("<button>").attr("data-movie");
 
-		newButton.attr("class", "btn btn-default");
+			newButton.attr("class", "btn btn-default");
 
-		$("#button").append(newButton);
+			$("#button").append(newButton);
+		}
 	}
 
 // When someone clicks on a button it will call ajax in order to pull data from the Giphy API.
@@ -25,8 +27,30 @@ $(document).ready(function() {
 	        url: queryURL,
 	        method: "GET"
 	      }).done(function(response) {
-	      	console.log(response);
+	      	var results = response.data;
+
+	      	updateDisplay();
+ 
+ 	      	for (var i = 0; i < results.length; i++) {
+ 	      		
+ 	      		var newDiv = $("<div class='item'>");
+ 
+ 		      	var newImg = $("<img>");
+ 
+ 		      	newImg.attr("src", results[i].images.fixed_height.url);
+ 		      	// newImg.attr("data-status");
+ 		      	// newImg.attr("data-animated");
+ 		      	// newImg.attr("data-still");
+ 
+ 		      	newDiv.append(newImg);
+ 
+ 		      	$("#gifs").prepend(newDiv);
+			}
 	      });
 
 	});
+
+	function() updateDisplay {
+		$(".item").empty();
+	}
 });
