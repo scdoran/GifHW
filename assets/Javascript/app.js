88fad28a43fc2a72d0a41ac4051df112a7c1ed07
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2017-01-14 17:04:43 -0500

    Got everything working. Now I can just tweak the visuals.

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,13 +1,14 @@
-$(document).ready(function() {
 
 	var movieArray= ["Re-Animator", "Evil Dead", "Texas Chainsaw Massacre", "The Fly", "They Live", "City of the Living Dead", "Samurai Cop", "Riki-Oh"];
 
-	// function newButton() {
+$(document).ready(function() {
+
+	function newButton() {
 		$("#buttons").empty();
 
 		for (var i = 0; i < movieArray.length; i++) {
 			
-			var newButton = $("<button class='btn btn-default'>");
+			var newButton = $("<button class='btn btn-default movies'>");
 
 			newButton.attr("data-movie", movieArray[i]);
 
@@ -15,21 +16,16 @@ $(document).ready(function() {
 
 			$("#buttons").append(newButton);
 		}
-	// }
+	}
 
-	$("#submit").on("click", function(event) {
-
-		var movie = $("#movieText").val();
-
-		movieArray.push(movie);
-
-		newButton();
-	});
-
-	$("button").on("click", function() {
+	function gifShow() {	
 		$("#gifs").empty();
 
 		var movie = $(this).attr("data-movie");
+
+		// var rating = "r"; 
+		// Would place "?rating=" + rating + in the URL below if I wanted to use it. 
+		// Adding this parameter messes with the results too much so I decided to keep it out.
 	    
 	    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
 	        movie + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -38,28 +34,64 @@ $(document).ready(function() {
 	        url: queryURL,
 	        method: "GET"
 	      }).done(function(response) {
-	      	console.log(response.data);
 
 	      	var results = response.data;
 
 	      	for (var i = 0; i < results.length; i++) {
 	      		
-	      		var newDiv = $("<div class='item'>");
+	      		var newDiv = $("<div class='item text-center'>");
 
 		      	var newImg = $("<img>");
 
-		      	newImg.attr("src", results[i].images.fixed_height.url);
-		      	// newImg.attr("data-status");
-		      	// newImg.attr("data-animated");
-		      	// newImg.attr("data-still");
+		      	newImg.addClass("gif");
+		      	newImg.attr("src", results[i].images.fixed_height_still.url);
+		      	newImg.attr("data-status", "still");
+		      	newImg.attr("data-animated", results[i].images.fixed_height.url);
+		      	newImg.attr("data-still", results[i].images.fixed_height_still.url);
 
 		      	newDiv.append(newImg);
+
+		      	var rating = results[i].rating;
+
+				var h3 = $("<h3>").text("GIF Rating: " + rating);
+
+				newDiv.append(h3);
 
 		      	$("#gifs").prepend(newDiv);
 	      	}
 	      });
+	}
+	
+	function changeStatus(event){
+					
+			var status = $(event.target).attr("data-status");
+
+			if (status === "still") {
+				$(this).attr("src", $(this).attr("data-animated"));
+		       $(this).attr("data-status", "animated");
+		       console.log("Animated!");
+		    } else{
+		       $(this).attr("data-status", "still");	
+		       $(this).attr("src", $(this).attr("data-still"));
+		       console.log("Still!");
+			}	
+	}
+
+	$("#submit").on("click", function() {
+		event.preventDefault();
+
+		var newMovie = $("#movieText").val().trim();
+
+		movieArray.push(newMovie);
+
+		$("#movieText").val(" ");
+
+		newButton();
 	});
 
-	// newButton();
+	$(document).on("click", ".movies", gifShow);
+	$(document).on("click", ".gif", changeStatus);
+
+	newButton();
 
 });
